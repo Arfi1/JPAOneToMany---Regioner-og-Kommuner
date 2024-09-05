40d5d2eea57fad036724d96f91125e3067ff8e73
arfi <arma0001@stud.kea.dk>
2024-09-05 11:31:40 +0200

    Implementering af post, put og delete i mine restcontrollers

--- src/main/java/com/example/jpaonetomany/controller/KommuneRESTController.java
+++ src/main/java/com/example/jpaonetomany/controller/KommuneRESTController.java
@@ -2,23 +2,60 @@ package com.example.jpaonetomany.controller;
 
 import com.example.jpaonetomany.model.Kommune;
 import com.example.jpaonetomany.model.Region;
+import com.example.jpaonetomany.repository.KommuneRepository;
 import com.example.jpaonetomany.service.ApiServiceGetKommuner;
 import com.example.jpaonetomany.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class KommuneRESTController {
 
     @Autowired
     ApiServiceGetKommuner apiServiceGetKommuner;
+    @Autowired
+    KommuneRepository kommuneRepository;
 
     @GetMapping("/getkommuner")
     public List<Kommune> getKommuner(){
         List<Kommune> lstKommuner = apiServiceGetKommuner.getKommuner();
         return lstKommuner;
     }
+
+    @PostMapping("/kommuner")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kommune postKommune(@RequestBody Kommune kommune) {
+        System.out.println(kommune);
+        return kommuneRepository.save(kommune);
+    }
+
+    @PutMapping("/kommuner/{kode}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Kommune> putKommune(@PathVariable String kode, @RequestBody Kommune kommune) {
+        Optional<Kommune> orgKommune = kommuneRepository.findById(kode);
+        if (orgKommune.isPresent()) {
+            kommuneRepository.save(kommune);
+            return ResponseEntity.ok(kommune);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/kommuner/{kode}")
+    public ResponseEntity<String> deleteKommune(@PathVariable String kode) {
+        Optional<Kommune> orgKommune = kommuneRepository.findById(kode);
+        if (orgKommune.isPresent()) {
+            kommuneRepository.deleteById(kode);
+            return ResponseEntity.ok("Kommune deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Kommune not found");
+        }
+    }
+
 }
--- src/main/java/com/example/jpaonetomany/controller/RegionRESTController.java
+++ src/main/java/com/example/jpaonetomany/controller/RegionRESTController.java
@@ -1,12 +1,16 @@
 package com.example.jpaonetomany.controller;
 
+import com.example.jpaonetomany.model.Kommune;
 import com.example.jpaonetomany.model.Region;
+import com.example.jpaonetomany.repository.RegionRepository;
 import com.example.jpaonetomany.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RegionRESTController {
@@ -14,9 +18,44 @@ public class RegionRESTController {
     @Autowired
     ApiServiceGetRegioner apiServiceGetRegioner;
 
+    @Autowired
+    RegionRepository regionRepository;
+
     @GetMapping("/getregioner")
     public List<Region> getRegioner(){
        List<Region> lstRegioner = apiServiceGetRegioner.getRegioner();
        return lstRegioner;
     }
+
+    @PostMapping("/regioner")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Region postRegion(@RequestBody Region region) {
+        System.out.println(region);
+        return regionRepository.save(region);
+    }
+
+    @PutMapping("/regioner/{kode}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Region> putRegion(@PathVariable String kode, @RequestBody Region region) {
+        Optional<Region> orgRegion = regionRepository.findById(kode);
+        if (orgRegion.isPresent()) {
+            regionRepository.save(region);
+            return ResponseEntity.ok(region);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/regioner/{kode}")
+    public ResponseEntity<String> deleteRegion(@PathVariable String kode) {
+        Optional<Region> orgRegion = regionRepository.findById(kode);
+        if (orgRegion.isPresent()) {
+            regionRepository.deleteById(kode);
+            return ResponseEntity.ok("Region deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Region not found");
+        }
+    }
+
 }
